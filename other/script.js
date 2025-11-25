b3b1b407c420bd70b2006e4b06fa82b29d2bb619
Gamer-XR637 <168220319+GamerXR-637@users.noreply.github.com>
2025-11-25 08:47:15 -0500

    Update script.js

--- other/script.js
+++ other/script.js
@@ -226,14 +226,15 @@ document.addEventListener('DOMContentLoaded', () => {
         ];
         const choice = avatarPool[Math.floor(Math.random() * avatarPool.length)];
         swapImageWithFade(mainAvatar, choice);
-    } else if (Math.random() >= 0.1) { // INCREASED CHANCE: Changed 0.4 to 0.1
-        const ymainAvatar = document.querySelector('.top-left-image');
-        const mainavatarPool = [
-            'other/glitch-pfp-rgb.png',
-            'other/more-glitch-pfp-rgb.png',
-            'other/new-pfp-axvill0.png'
-        ];
-        const mchoice = mainavatarPool[Math.floor(Math.random() * mainavatarPool.length)];
-        swapImageWithFade(ymainAvatar, mchoice);
-    }
+    } // else if (Math.random() >= 0.1) { // INCREASED CHANCE: Changed 0.4 to 0.1
+    //     const ymainAvatar = document.querySelector('.top-left-image');
+    //     const mainavatarPool = [
+    //         'other/glitch-pfp-rgb.png',
+    //         'other/more-glitch-pfp-rgb.png',
+    //         'other/new-pfp-axvill0.png'
+    //     ];
+    //     const mchoice = mainavatarPool[Math.floor(Math.random() * mainavatarPool.length)];
+    //     swapImageWithFade(ymainAvatar, mchoice);
+    // }
 });
+
